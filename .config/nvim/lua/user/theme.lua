57acf8bfea0012b362fc03a021f7663b5a9d7b56
Artur Balabanov <arturbalabanov97@gmail.com>
2023-06-25 13:31:11 +0100

    Updated dotfiles 2023-06-25 13:31 BST from Work

--- .config/nvim/lua/user/theme.lua
+++ .config/nvim/lua/user/theme.lua
@@ -1,63 +1,46 @@
 local status_ok, gruvbox = pcall(require, "gruvbox")
 if not status_ok then
-    return
+  return
 end
 
 local palette = require("gruvbox.palette")
 
 gruvbox.setup({
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-        strings = false,
-        operators = false,
-        comments = true,
-    },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true,    -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {
-        SignColumn = { bg = palette.colors.dark0_hard },
-        GruvboxRedSign = { bg = palette.colors.dark0_hard },
-        GruvboxGreenSign = { bg = palette.colors.dark0_hard },
-        GruvboxYellowSign = { bg = palette.colors.dark0_hard },
-        GruvboxBlueSign = { bg = palette.colors.dark0_hard },
-        GruvboxPurpleSign = { bg = palette.colors.dark0_hard },
-        GruvboxAquaSign = { bg = palette.colors.dark0_hard },
-        GruvboxOrangeSign = { bg = palette.colors.dark0_hard },
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = false,
+    operators = false,
+    comments = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true,      -- invert background for search, diffs, statuslines and errors
+  contrast = "hard",   -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {
+    SignColumn = { bg = palette.colors.dark0_hard },
+    GruvboxRedSign = { bg = palette.colors.dark0_hard },
+    GruvboxGreenSign = { bg = palette.colors.dark0_hard },
+    GruvboxYellowSign = { bg = palette.colors.dark0_hard },
+    GruvboxBlueSign = { bg = palette.colors.dark0_hard },
+    GruvboxPurpleSign = { bg = palette.colors.dark0_hard },
+    GruvboxAquaSign = { bg = palette.colors.dark0_hard },
+    GruvboxOrangeSign = { bg = palette.colors.dark0_hard },
 
-        NeotestFailed = { fg = palette.colors.neutral_red },
-        NeotestPassed = { fg = palette.colors.neutral_green },
-        NeotestRunning = { fg = palette.colors.neutral_blue },
-        NeotestSkipped = { fg = palette.colors.neutral_yellow },
-        --
-        -- adapter_name = "NeotestAdapterName",
-        -- border = "NeotestBorder",
-        -- dir = "NeotestDir",
-        -- expand_marker = "NeotestExpandMarker",
-        -- failed = "NeotestFailed",
-        -- file = "NeotestFile",
-        -- focused = "NeotestFocused",
-        -- indent = "NeotestIndent",
-        -- marked = "NeotestMarked",
-        -- namespace = "NeotestNamespace",
-        -- passed = "NeotestPassed",
-        -- running = "NeotestRunning",
-        -- select_win = "NeotestWinSelect",
-        -- skipped = "NeotestSkipped",
-        -- target = "NeotestTarget",
-        -- test = "NeotestTest",
-        -- unknown = "NeotestUnknown",
-        -- watching = "NeotestWatching"
-    },
-    dim_inactive = false,
-    transparent_mode = false,
+    NeotestFailed = { fg = palette.colors.neutral_red },
+    NeotestPassed = { fg = palette.colors.neutral_green },
+    NeotestRunning = { fg = palette.colors.neutral_blue },
+    NeotestSkipped = { fg = palette.colors.neutral_yellow },
+
+    GitSignsCurrentLineBlame = { fg = palette.colors.gray },
+  },
+  dim_inactive = false,
+  transparent_mode = false,
 })
 
 vim.cmd.colorscheme("gruvbox")
